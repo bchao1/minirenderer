c6be71c67cc245d9814877f2323343ac8c94b28d
bchao1 <whereisbrianchao520@gmail.com>
2020-07-19 21:41:59 +0800

    fix gif script

--- gif.py
+++ gif.py
@@ -1,5 +1,6 @@
 import numpy as np
 from renderer.renderer import WireframeRenderer
+from PIL import ImageOps
 
 if __name__ == '__main__':
     mode = 'triangle'
@@ -10,6 +11,7 @@ if __name__ == '__main__':
     wf.readfile(inpath)
 
     im_list = []
+    max_w, max_h = -np.inf, -np.inf
     for i in range(72):
         r = 100
         theta = 5 * i
@@ -19,6 +21,10 @@ if __name__ == '__main__':
         up = [0, 1, 0]
 
         canvas = wf.render(mode, camera, center, up)
+        w, h = canvas.size
+        max_w = max(w, max_w)
+        max_h = max(h, max_h)
         im_list.append(canvas.img)
 
+    im_list = [ImageOps.pad(img, (max_w, max_h), color=(255,255,255)) for img in im_list]
     im_list[0].save('images/{}_render.gif'.format(obj), save_all=True, append_images=im_list[1:], duration=100, loop=0)
